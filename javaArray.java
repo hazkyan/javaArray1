01b44c7ec0237bd7c11e294c0abce9364f221065
Keanu <hazpoldev@gmail.com>
2024-11-03 17:48:55 +0800

    added file

---
+++ javaArray.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class javaArray {
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        String[] IDnum = {"2022-001", "2022-002", "2022-003", "2020-004"};
+        String[] course = {"BSIT", "BSCS", "BSIS", "BSEMC"};
+        String[] name = {"James Cooper", "Princess Smith", "Dianna Ross", "James Butler"};
+        String[] sports = {"Basketball", "Badminton", "Swimming", "Baseball"};
+        String[] contact = {"0920864756", "09178765454", "09167657382", "09108877635"};
+        
+        System.out.print("Students Name: ");
+           for (int i = 0; i < name.length; i++) {
+              System.out.print(name[i]);
+           if (i < name.length - 1) {
+            System.out.print(", ");
+        }
+    }
+
+    System.out.print("\nEnter Student Index: ");
+    int index = scan.nextInt();
+    scan.nextLine();
+if (index >= 0 && index <= 3) {
+System.out.print("\nIndex Number\t: " + index);
+System.out.print("\nStudent ID\t: " + IDnum[index]);
+System.out.print("\nName of Student\t: " + name[index]);
+System.out.print("\nCourse\t\t: " + course[index]);
+System.out.print("\nSports\t\t: " + sports[index]);
+System.out.print("\nContact No.\t: " + contact[index]);
+} else {
+System.out.println("Student not found!");
+}
+scan.close();
+System.out.println();
+}
+}
